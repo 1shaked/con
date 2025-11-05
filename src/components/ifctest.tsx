f87163bc495a3a5dbe1bd8d2247a2a977108dc89
1shaked <shaked1hen@gmail.com>
2025-11-05 14:42:22 +0200

    fix: enhance IfcTest component initialization and integrate BUI for improved UI interactions

--- src/components/ifctest.tsx
+++ src/components/ifctest.tsx
@@ -6,7 +6,7 @@ import * as BUI from "@thatopen/ui";
 
 export function IfcTest() {
     const containerRef = useRef(null);
-
+    const isInitializedRef = useRef(false);
     useEffect(() => {
         async function init() {
             const components = new OBC.Components();
@@ -19,23 +19,21 @@ export function IfcTest() {
 
 
             world.scene = new OBC.SimpleScene(components);
-
-            world.scene?.setup()
+            
             if (!containerRef?.current) return;
-            world.scene = new OBC.SimpleScene(components);
             world.scene.setup();
             world.scene.three.background = null;
             world.renderer = new OBC.SimpleRenderer(components, containerRef?.current);
             world.camera = new OBC.OrthoPerspectiveCamera(components);
             await world.camera.controls.setLookAt(78, 20, -2.2, 26, -4, 25);
-            components.init();
             // Setup Grid
             const grids = components.get(OBC.Grids);
             grids.create(world);
-
-
+            components.init();
+            
             const fragments = components.get(OBC.FragmentsManager);
             fragments.init("/src/worker.mjs");
+            
             // set fragment that when a model is added, it is added to the scene and linked to the camera
             fragments.list.onItemSet.add(({ value: model }) => {
                 model.useCamera(world.camera.three);
@@ -62,8 +60,120 @@ export function IfcTest() {
                 if (!file) return;
                 await loadIFCFile(file, ifcLoader, file.name);
             });
+
+            const finder = components.get(OBC.ItemsFinder);
+            finder.create("WALL", [{ categories: [/WALL/] }]);
+            finder.create("DOOR", [{ categories: [/DOOR/, /WINDOW/, /SLAB/] }]);
+            finder.create("WINDOW", [{ categories: [/WINDOW/, /SLAB/] }]);
+            finder.create("SLAB", [{ categories: [/SLAB/] }]);
+
+
+
+
+            // add BUI
+            // if (!BUI.Manager.initialized) BUI.Manager.init();
+            if (!isInitializedRef.current) {
+                // debugger
+                BUI.Manager.init();
+                isInitializedRef.current = true;
+            }
+
+            type QueriesListTableData = {
+                Name: string;
+                Actions: string;
+            };
+
+            function queriesListTemplate(finder: OBC.ItemsFinder) {
+                const onCreated = (e?: Element) => {
+                    if (!e) return;
+                    const table = e as BUI.Table<QueriesListTableData>;
+
+                    table.loadFunction = async () => {
+                        const data: BUI.TableGroupData<QueriesListTableData>[] = [];
+
+                        for (const [name] of finder.list) {
+                            data.push({
+                                data: { Name: name, Actions: "" },
+                            });
+                        }
+
+                        return data;
+                    };
+
+                    table.loadData(true);
+                };
+
+                return BUI.html`
+                    <bim-table ${BUI.ref(onCreated)}></bim-table>
+                `;
+            };
+
+            const queriesList =
+                BUI.Component.create<BUI.Table<QueriesListTableData>>(() => queriesListTemplate(finder));
+
+            queriesList.style.maxHeight = "25rem";
+            queriesList.columns = ["Name", { name: "Actions", width: "auto" }];
+            queriesList.noIndentation = true;
+            queriesList.headersHidden = true;
+            queriesList.dataTransform = {
+                Actions: (_, rowData) => {
+                    const { Name } = rowData;
+                    if (!Name) return _;
+
+                    const hider = components.get(OBC.Hider);
+                    const onClick = async ({ target }: { target: BUI.Button }) => {
+                        target.loading = true;
+                        const modelIdMap = await getResult(Name, finder);
+                        await hider.isolate(modelIdMap);
+                        target.loading = false;
+                    };
+
+                    return BUI.html`<bim-button icon="solar:cursor-bold" @click=${onClick}></bim-button>`;
+                },
+            };
+
+            const panel = BUI.Component.create<BUI.PanelSection>(() => {
+                const onResetVisibility = async ({ target }: { target: BUI.Button }) => {
+                    target.loading = true;
+                    const hider = components.get(OBC.Hider);
+                    await hider.set(true);
+                    target.loading = false;
+                };
+
+                return BUI.html`
+                    <bim-panel active label="Items Finder Tutorial" class="options-menu">
+                    <bim-panel-section style="min-width: 14rem" label="General">
+                        <bim-button label="Reset Visibility" @click=${onResetVisibility}></bim-button>
+                    </bim-panel-section>
+                    <bim-panel-section label="Queries">
+                        ${queriesList}
+                    </bim-panel-section>
+                    </bim-panel>
+                `;
+            });
+            // check if panel already exists in the document
+            const existingPanel = document.body.querySelector('.options-menu');
+            if (existingPanel) {
+                existingPanel.remove();
+            }
+            document.body.append(panel);
+            
+            return {components, world, panel};
+
         }
-        init();
+        const items = init();
+        return () => {
+            items.then((objs) => {
+                objs?.components?.dispose();
+                // objs?.world?.dispose();
+                objs?.panel?.remove();
+                if (objs?.panel?.parentElement) {
+                    document.body.removeChild(objs?.panel.parentElement);
+                }
+            });
+        }
+
+
     }, []);
 
     return (
@@ -76,9 +186,9 @@ export function IfcTest() {
                     block w-full max-w-sm cursor-pointer rounded-xl shadow-sm
                     text-sm text-slate-900
                     file:mr-4 file:rounded-xl file:border-0 file:px-4 file:py-2
-                    file:font-semibold file:bg-amber-600 file:text-white
-                    hover:file:bg-amber-500
-                    focus:outline-none focus:ring-2 focus:ring-amber-500/60
+                    file:font-semibold file:bg-blue-600 file:text-white
+                    hover:file:bg-blue-500
+                    focus:outline-none focus:ring-2 focus:ring-blue-500/60
                     disabled:opacity-50
                 "
             />
@@ -97,4 +207,11 @@ export async function loadIFCFile(file: File, ifcLoader: OBC.IfcLoader, name: st
             progressCallback: (progress) => console.log(progress),
         },
     });
-} 
+}
+
+export async function getResult(name: string, finder: OBC.ItemsFinder) {
+    const finderQuery = finder.list.get(name);
+    if (!finderQuery) return {};
+    const result = await finderQuery.test();
+    return result;
+};
