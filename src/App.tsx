848bca16c1cdd009fdb3c9962233c17d8370920a
1shaked <shaked1hen@gmail.com>
2025-11-06 14:29:55 +0200

    feat: wrap App component with QueryClientProvider for React Query integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { IfcTest } from "./components/ifctest"
 // import { IfcViewer } from "./components/ifcviewr"
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="w-full">
         <IfcTest />
       </div>
-    </>
+    </QueryClientProvider>
   )
 }
 
