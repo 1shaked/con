2b20c4a4a82958b144b0aa57cd0d335ef51b382e
1shaked <shaked1hen@gmail.com>
2025-11-06 12:55:41 +0200

    feat: update type references in queriesListTemplate for consistency

--- src/utils/Queries_List_Template.ts
+++ src/utils/Queries_List_Template.ts
@@ -1,14 +1,14 @@
 import { ItemsFinder } from "@thatopen/components";
-import type { QueriesListTableData } from "../types/QueriesListTableData";
+import type { TQueriesListTableData } from "../types/QueriesListTableData";
 import { Table, type TableGroupData as TypeTableGroupData, html as BUIHtml, ref as BUIRef } from "@thatopen/ui";
 
 export function queriesListTemplate(finder: ItemsFinder) {
     const onCreated = (e?: Element) => {
         if (!e) return;
-        const table = e as Table<QueriesListTableData>;
+        const table = e as Table<TQueriesListTableData>;
 
         table.loadFunction = async () => {
-            const data: TypeTableGroupData<QueriesListTableData>[] = [];
+            const data: TypeTableGroupData<TQueriesListTableData>[] = [];
 
             for (const [name] of finder.list) {
                 data.push({
