5cbd1d74a500d774f7b11b6d07ea2a95f0294117
1shaked <shaked1hen@gmail.com>
2025-11-06 23:17:46 +0200

    feat: allow Level field to be nullable in IFC_Table_Row_Schema

--- src/validators/IFC_Table_Row_Schema.ts
+++ src/validators/IFC_Table_Row_Schema.ts
@@ -4,7 +4,7 @@ export const IFC_Table_Row_Schema = z.object({
     Element_Type: z.string(),
     Unit: z.string(),
     Project_Total: z.number(),
-    Level: z.string(),
+    Level: z.string().nullable(),
     Quantity: z.number()
 });
 
