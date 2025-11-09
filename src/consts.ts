8a74cc6925af84e09a0c1d625c9d587cabce8b6d
1shaked <shaked1hen@gmail.com>
2025-11-09 02:56:24 +0200

    feat: add color constants and update URL_SERVER in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,21 @@ import {Color} from "three";
 
 export const COLOR_PURPLE = new Color("purple");
 export const URL_SERVER = 'http://127.0.0.1:8000/';
+
+export const COLORS_SET = [
+    "#062c5b",       // '#062c5b'
+    "#fee7b1",   // '#fee7b1'
+    "#4c2c15",      // '#4c2c15'
+    "#a89dde",   // '#a89dde'
+    "#9dd3de",   // '#9dd3de'
+    "#828961",    // '#828961'
+    "#3a3c32",      // '#3a3c32'
+    "#c3c3c3",   // '#c3c3c3'
+    "#d6612b",     // '#d6612b'
+    "#73e6aa",   // '#73e6aa'
+    "#83a5f9",   // '#83a5f9'
+    "#ad56f9",    // '#ad56f9'
+    "#f01236",     // '#f01236'
+    "#f9d423",    // '#f9d423'
+    "#cccccc",   // '#cccccc'
+];
