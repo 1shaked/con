cbf58c444b29dfe9592ecbfb36d37b3e27c5157a
1shaked <shaked1hen@gmail.com>
2025-11-09 02:56:30 +0200

    feat: integrate color constants and update element highlighting logic in IfcTest component

--- src/components/ifctest.tsx
+++ src/components/ifctest.tsx
@@ -10,28 +10,12 @@ import { loadIFCFile } from '../utils/Load_IFC_File';
 // import type { TQueriesListTableData } from '../types/QueriesListTableData';
 // import { queriesListTemplate } from '../utils/Queries_List_Template';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { URL_SERVER } from '../consts';
+import { COLORS_SET, URL_SERVER } from '../consts';
 import { Server_File_Info_Schema } from '../validators/Server_File_Info_Schema';
 import { Server_GUIDS_For_Type_Schema } from '../validators/Server_GUIDS_For_Type';
 import type { ElementSelectionParams } from '../types/ElementSelectionParams';
+import { hexToRgba } from '../utils/Hex_To_RGBA';
 
-const COLORS_SET = [
-    "rgba(6, 44, 91, 0.5)",       // '#062c5b'
-    "rgba(254, 231, 177, 0.5)",   // '#fee7b1'
-    "rgba(76, 44, 21, 0.5)",      // '#4c2c15'
-    "rgba(168, 157, 222, 0.5)",   // '#a89dde'
-    "rgba(157, 211, 222, 0.5)",   // '#9dd3de'
-    "rgba(130, 137, 97, 0.5)",    // '#828961'
-    "rgba(58, 60, 50, 0.5)",      // '#3a3c32'
-    "rgba(195, 195, 195, 0.5)",   // '#c3c3c3'
-    "rgba(214, 97, 43, 0.5)",     // '#d6612b'
-    "rgba(115, 230, 170, 0.5)",   // '#73e6aa'
-    "rgba(131, 165, 249, 0.5)",   // '#83a5f9'
-    "rgba(173, 86, 249, 0.5)",    // '#ad56f9'
-    "rgba(240, 18, 54, 0.5)",     // '#f01236'
-    "rgba(249, 212, 35, 0.5)",    // '#f9d423'
-    "rgba(204, 204, 204, 0.5)",   // '#cccccc'
-];
 
 
 export function IfcTest() {
@@ -181,6 +165,7 @@ export function IfcTest() {
             setSelectedElements(new Set([...selectedElements, key]));
             // select a color from the color set based on the number of selected elements
             const colorIndex = selectedElements.size % COLORS_SET.length;
+            
             const color = COLORS_SET[colorIndex];
             await fragmentsRef.current?.highlight({
                 color: new THREE.Color(color),
@@ -268,13 +253,13 @@ export function IfcTest() {
                             {model_data.data?.data.map((row, index) => (
                                 <div
                                     key={`${row.Element_Type}-${row.Level}`}
-                                    className={`group bg-linear-to-br from-gray-50 to-gray-100
+                                    className={` bg-linear-to-br from-gray-50 to-gray-100 group 
                                              border border-gray-200 hover:border-blue-300 rounded-lg p-4 
                                              transform hover:scale-105 transition-all duration-200 
                                              hover:shadow-md cursor-pointer
-                                             ${selectedElements.has(`${row.Element_Type}-${row.Level}`) ? `shadow-lg shadow-blue-500/25 bg-[${COLORS_SET[index % COLORS_SET.length]}]` : ''}`}
+                                             ${selectedElements.has(`${row.Element_Type}-${row.Level}`) ? `shadow-lg shadow-blue-500/25 text-[${COLORS_SET[index % COLORS_SET.length]}]` : ''}`}
                                 style={{
-                                    background: selectedElements.has(`${row.Element_Type}-${row.Level}`) ? COLORS_SET[index % COLORS_SET.length] : '',
+                                    background: selectedElements.has(`${row.Element_Type}-${row.Level}`) ? hexToRgba(COLORS_SET[index % COLORS_SET.length], 0.6) : '',
                                 }}
                                 >
                                     <div className="flex items-start justify-between mb-3">
@@ -282,7 +267,7 @@ export function IfcTest() {
                                             <h4 className={`font-semibold text-gray-900 group-hover:text-blue-700 
                                                          text-lg leading-tight ${selectedElements.has(`${row.Element_Type}-${row.Level}`) ? 'text-blue-700' : ''}`}
                                             >
-                                                {row.Element_Type} {COLORS_SET[index % COLORS_SET.length]}
+                                                {row.Element_Type} 
                                             </h4>
                                             <div className="flex items-center gap-2 mt-2">
                                                 <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full 
